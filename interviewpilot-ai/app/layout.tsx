7e0500d9800da0b3f4be1938d0389e89ea2957f6
dev_nick <developernikhil28@gmail.com>
2026-02-13 13:09:18 +0530

    chore(init): initialize Next.js application

---
+++ interviewpilot-ai/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: "Real-time AI voice interview SaaS built with Next.js, Firebase, Vapi AI, and Google Gemini. Features adaptive interview generation, transcript analysis, and performance analytics dashboard.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
